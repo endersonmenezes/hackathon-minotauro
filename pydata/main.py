ea5513e9fa53a17ac63e2e351ff29c5468600e67
Gustavo Mendonça <gustavo.dias@iadtec.com.br>
2021-04-10 15:28:39 -0300

    alteracoes python

--- pydata/main.py
+++ pydata/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from solution import solution_maze
 from models.maze import Maze
 
@@ -8,13 +8,18 @@ app = Flask(__name__)
 
 @app.route('/generateMaze', methods=['GET'])
 def generate_maze():
-    width = 20
-    height = 20
+    # largura = int(request.args.get('largura', False))
+    # altura = int(request.args.get('altura', False))
+    quantidade = int(request.args.get('quantidade'), False)
+    if not quantidade:
+        quantidade = 10
     start = x, y = 1, 1
     zoom = 20
     borders = 6
-    end = 19, 19
-    maze = Maze(width, height)
+    end_x = quantidade - 1
+    end_y = quantidade - 1
+    end = end_x, end_y
+    maze = Maze(quantidade, quantidade)
     maze.create_maze(x, y)
     maze.cells[1][0] = False
     data = solution_maze(maze.cells, start, end, zoom, borders)
