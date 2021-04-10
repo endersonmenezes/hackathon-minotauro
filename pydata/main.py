ce88838fe0e03af1946ed0811d0bca7b8a037abf
Enderson Menezes <endersonster@gmail.com>
2021-04-10 16:14:06 -0300

    att

--- pydata/main.py
+++ pydata/main.py
@@ -11,6 +11,7 @@ def generate_maze():
     # largura = int(request.args.get('largura', False))
     # altura = int(request.args.get('altura', False))
     quantidade = request.args.get('quantidade', False)
+    id_partida = request.args.get('id_partida', False)
     if quantidade:
         quantidade = int(quantidade)
     else:
@@ -24,7 +25,7 @@ def generate_maze():
     maze = Maze(quantidade, quantidade)
     maze.create_maze(x, y)
     maze.cells[1][0] = False
-    data = solution_maze(maze.cells, start, end, zoom, borders)
+    data = solution_maze(maze.cells, start, end, zoom, borders, id_partida)
     return jsonify(data)
 
 @app.route('/map/<string:file_type>', methods=['GET'])
