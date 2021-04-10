0d6c0eb2a09b290fc40782b8698d4dcddacef621
Moraes <pedro.moraes@accountfy.com>
2021-04-10 14:32:15 -0300

    ajustes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
 import { RoomComponent } from './room/room.component';
-import { CommonModule } from '@angular/common';
+import { ControlComponent } from './control/control.component';
 
-import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, RoomComponent],
-  imports: [BrowserModule, CommonModule, HttpClientModule],
+  declarations: [AppComponent, RoomComponent, ControlComponent],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
